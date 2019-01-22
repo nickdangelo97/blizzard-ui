ede7c00632f5e98ad774f4bbafff026419615365
Nick <dangelon@mcmaster.ca>
2019-01-21 19:50:33 -0500

    Add Drawer button to toolbar

--- src/components/Navigation/Toolbar.js
+++ src/components/Navigation/Toolbar.js
@@ -1,32 +1,38 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import Logo from '../../assets/torontoblizzard.png'
+import { IconButton } from '@material-ui/core';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
   },
   appbar: {
-      height: "56px",
-      position: "fixed",
-      "top": "0",
-      "left": "0",
-      "backgroundColor": "#C01237",
-      "display": "flex",
-      "justifyContent": "space-between",
-      "alignItems": "center",
-      "padding": "0 20px",
-       zIndex: theme.zIndex.drawer + 1,
+    height: "56px",
+    position: "fixed",
+    top: "0",
+    left: "0",
+    backgroundColor: theme.palette.primary.main,
+    padding: "0 20px",
+    zIndex: theme.zIndex.drawer + 1,
   },
 
-  logo : {
-      height: "80px",
-      marginTop: "20px"
+  toolbar: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center"
+
+  },
+
+  logo: {
+    height: "80px",
+    marginTop: "20px",
+    marginRight: "auto"
   },
 });
 
@@ -36,8 +42,11 @@ function SimpleAppBar(props) {
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appbar}>
-        <Toolbar>
-        <img src={Logo} className={classes.logo}/>
+        <Toolbar className={classes.toolbar}>
+          <IconButton color="inherit" style={{ marginRight: "auto" }}>
+            <MenuIcon />
+          </IconButton>
+          <img src={Logo} className={classes.logo} />
         </Toolbar>
       </AppBar>
     </div>
