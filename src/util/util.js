e4a792509b12757fc353ebdee686665f7e5527b6
Nick <dangelon@mcmaster.ca>
2019-03-24 19:37:22 -0400

    util: add baseUrl

--- src/util/util.js
+++ src/util/util.js
@@ -1,3 +1,11 @@
-export const getAccessString = () => {
+const getAccessString = () => {
     return "Bearer " + sessionStorage.getItem("token")
 }
+
+const baseUrl = "http://ec2-3-16-167-88.us-east-2.compute.amazonaws.com:3000"
+
+export {
+    getAccessString,
+    baseUrl
+}
+
